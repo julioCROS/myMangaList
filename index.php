5ed2e96ed8f702f27a665a4eb33a1466b8b5bb6c
Julio Cesar Rocha <julio.1009@hotmail.com>
2021-08-17 18:30:53 -0300

    Inicio do projeto

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <title>MyMangaList.net - Manga Database Community</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="src/styles/index.css">
+  </head>
+
+  <body>
+  <div class="horizontalSpace"></div>
+  <div class="body">
+    <div class="menuTop">
+      <div class="title">MyMangaList</div>
+      <div class="loginBtn"><strong><div class="textBtn1"><a href="/login.php">Login</a></div></strong></div>
+      <div class="signUpBtn"><strong><div class="textBtn2"><a href="/sign-up.php" style="color:white">Sign Up</a></strong></div>
+    </div>    
+  </div>
+  </body>
+  <div class="horizontalSpace"></div>
+  
+</html>
+
+
+
+
