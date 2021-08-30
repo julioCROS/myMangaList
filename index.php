e7c2a388112490f477ec7448eec06b845d8c5436
Julio Cesar Rocha <julio.1009@hotmail.com>
2021-08-30 13:38:28 -0300

    Update da estlização da pag. inicial

--- index.php
+++ index.php
@@ -4,32 +4,33 @@
 <head>
   <title>MyMangaList.net - Personal Manga Database</title>
   <meta charset="utf-8">
-  <link rel="stylesheet" href="src\styles\index-style.css">
+  <link rel="stylesheet" href="src\styles\index_style.css">
 </head>
 
 
 <body>
   <div class="body">
-    <div class="menuTop">
-      <div class="title">MyMangaList</div>
-      <div class="loginBtn"><strong>
-          <div class="textBtn1"><a href="/login.php" style="color: #1d439b; text-decoration: none">Login</a></div>
-        </strong></div>
-      <div class="signUpBtn"><strong>
-          <div class="textBtn2"><a href="/sign-up.php" style="color:white; text-decoration: none">Sign Up</a>
-        </strong></div>
+    <div class="content">
+      <div class="menuTop">
+        <div class="title">MyMangaList</div>
+        <div class="loginBtn"><strong>
+            <div class="textBtn1"><a href="/login.php" style="color: #1d439b; text-decoration: none">Login</a></div>
+          </strong></div>
+        <div class="signUpBtn"><strong>
+            <div class="textBtn2"><a href="/sign-up.php" style="color:white; text-decoration: none">Sign Up</a>
+          </strong></div>
+      </div>
     </div>
-  </div>
 
-  <div class="menuBar"></div>
-  <div class="containerContent">
-    <div class="welcomeText">
-      <p>
-        <?php
-        if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
-          echo "<p style='color:red;'>" . "Welcome $username to MyMangaList.net!" . "</p>";
-        } else {
-          echo "<p style='color:black;
+    <div class="menuBar"></div>
+    <div class="containerContent">
+      <div class="welcomeText">
+        <p>
+          <?php
+          if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+            echo "<p style='color:red;'>" . "Welcome $username to MyMangaList.net!" . "</p>";
+          } else {
+            echo "<p style='color:black;
           font-size: 16px; 
           padding: 5px 9px; 
           font-weight: 700;
@@ -41,34 +42,34 @@
           border-width: 1px;
           background-color: #e1e7f5;
           font-family: Verdana, Geneva, Tahoma, sans-serif;'>" . "Welcome to MyMangaList.net!" . "</p>";
-        }
-        ?>
-      </p>
-    </div>
-    <?php
-    if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
-      echo "<p style='color:red;'>" . "Welcome $username" . "</p>";
-    } else {
-      echo "<p style='color:black;
+          }
+          ?>
+        </p>
+      </div>
+      <?php
+      if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+        echo "<p style='color:red;'>" . "Welcome $username" . "</p>";
+      } else {
+        echo "<p style='color:black;
           font-size: 40px;
           text-align: center;
           padding: 5px 9px; 
           font-weight: 30;
           font-family: Verdana, Geneva, Tahoma, sans-serif;'>" . "Please Log In to see the web-page." . "</p>";
-      echo "<p style='color:black;
+        echo "<p style='color:black;
           font-size: 20px;
           text-align: center;
           margin-top: -30px;
           font-weight: 30;
-          font-family: Verdana, Geneva, Tahoma, sans-serif;'>" . "You can sign-up if you don't have an account, it's simple." . "</p>";
-    }
-    ?>
+          font-family: Verdana, Geneva, Tahoma, sans-serif;'>" . "You can sign-up if you don't have an account." . "</p>";
+      }
+      ?>
+    </div>
   </div>
 </body>
 
 <footer>
 
 </footer>
-
 
 </html>
